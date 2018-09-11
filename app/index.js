fb2c08ec238243e04650d75081f7db3f2f58efed
Joel Balcaen <jobalcaen@gmail.com>
2018-09-11 10:33:39 -0400

    getting the project setup

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './components/Main';
+import './style.css';
+
+const appRoot = document.getElementById('root');
+
+ReactDOM.render(
+	<Main />,
+	appRoot
+);
+
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+let HTMLWebpackPlugin = require(
+	'html-webpack-plugin'
+);
+
+let HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+	template: __dirname + '/app/index.html',
+	filename: 'index.html',
+	inject: 'body'
+});
+
+module.exports = {
+	entry: __dirname + '/app/index.js',
+	mode: 'production',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: 'babel-loader'
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader','css-loader']
+			},
+			{
+	            test: /\.(png|jp(e*)g|svg)$/,  
+	            use: [{
+	                loader: 'url-loader',
+	                options: { 
+	                    limit: 8000, // Convert images < 8kb to base64 strings
+	                    name: 'images/[hash]-[name].[ext]'
+	                } 
+	            }]
+	        }
+		]
+	},
+	output: {
+		filename: 'transformed.js',
+		path: __dirname + '/build',
+	    publicPath: '/'
+
+	},
+	plugins: [
+		HTMLWebpackPluginConfig,
+	],
+	devServer: {
+    	host: '0.0.0.0',
+   	    historyApiFallback: true
+	}
+};
